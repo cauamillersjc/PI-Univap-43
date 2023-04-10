ed30dca38b18fef6623b0ddb5233f4e6ac575944
fsbispo <felipesbispo@gmail.com>
2023-04-10 20:36:15 -0300

    Fix: Add auto increment id and not-null on foreign keys

--- market_pdv.sql
+++ market_pdv.sql
@@ -1,105 +1,43 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Tempo de geração: 23-Mar-2023 às 00:09
--- Versão do servidor: 10.4.27-MariaDB
--- versão do PHP: 8.0.25
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Banco de dados: `market_pdv`
---
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `products`
---
-
-CREATE TABLE `products` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `description` varchar(255) NOT NULL,
-  `stock_quantity` bigint(20) NOT NULL,
-  `ean` varchar(255) NOT NULL,
-  `sku` varchar(255) NOT NULL,
-  `price` float NOT NULL,
-  `status` int(11) NOT NULL
+CREATE TABLE products(
+  id int(11) NOT NULL AUTO_INCREMENT,
+  description varchar(255) NOT NULL,
+  stock_quantity bigint(20) NOT NULL,
+  ean varchar(255) NOT NULL,
+  sku varchar(255) NOT NULL,
+  price float NOT NULL,
+  status int NOT NULL,
+  PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
 
---
--- Estrutura da tabela `sales`
---
 
-CREATE TABLE `sales` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `total_price` double NOT NULL,
-  `total_quantity` int(11) NOT NULL,
-  `sale_datetime` datetime NOT NULL,
-  `payment_method` varchar(255) NOT NULL
+CREATE TABLE sales (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  product_id int(11) NOT NULL,
+  price float NOT NULL,
+  total_price float NOT NULL,
+  discount double NOT NULL,
+  total_quantity int(11) NOT NULL,
+  sale_datetime datetime NOT NULL,
+  payment_method varchar(255) NOT NULL,
+  PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
 
---
--- Estrutura da tabela `sale_products`
---
 
-CREATE TABLE `sale_products` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `product_id` int(11) DEFAULT NULL,
-  `sale_id` int(11) DEFAULT NULL,
-  `product_quantity` int(11) NOT NULL
+CREATE TABLE sale_product (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  product_id int(11) NOT NULL,
+  sale_id int(11) NOT NULL,
+  product_quantity int(11) NOT NULL,
+  product_price float NOT NULL,
+  PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Índices para tabelas despejadas
---
+ALTER TABLE sales
+  ADD CONSTRAINT sale_ibfk_1 FOREIGN KEY (product_id) REFERENCES products (id);
 
---
--- Índices para tabela `products`
---
-ALTER TABLE `products`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE sale_product
+  ADD CONSTRAINT sale_product_ibfk_1 FOREIGN KEY (product_id) REFERENCES products (id),
+  ADD CONSTRAINT sale_product_ibfk_2 FOREIGN KEY (sale_id) REFERENCES sales (id);
 
---
--- Índices para tabela `sales`
---
-ALTER TABLE `sales`
-  ADD PRIMARY KEY (`id`);
-
---
--- Índices para tabela `sale_products`
---
-ALTER TABLE `sale_products`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `product_id` (`product_id`),
-  ADD KEY `sale_id` (`sale_id`);
-
---
--- Restrições para despejos de tabelas
---
-
---
--- Limitadores para a tabela `sale_products`
---
-ALTER TABLE `sale_products`
-  ADD CONSTRAINT `sale_products_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`),
-  ADD CONSTRAINT `sale_products_ibfk_2` FOREIGN KEY (`sale_id`) REFERENCES `sales` (`id`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
