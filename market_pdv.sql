fee036e0cdf93bb690b0f308ff48f993b98ba568
Felipe dos Santos Bispo <101956482+fsbispo@users.noreply.github.com>
2023-04-14 21:47:50 -0300

    fix: Adjust foreign key table sales

--- market_pdv.sql
+++ market_pdv.sql
@@ -13,7 +13,7 @@ CREATE TABLE products(
 
 CREATE TABLE sales (
   id int(11) NOT NULL AUTO_INCREMENT,
-  product_id int(11) NOT NULL,
+  ean String NOT NULL,
   price float NOT NULL,
   total_price float NOT NULL,
   discount double NOT NULL,
@@ -35,7 +35,7 @@ CREATE TABLE sale_product (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 ALTER TABLE sales
-  ADD CONSTRAINT sale_ibfk_1 FOREIGN KEY (product_id) REFERENCES products (id);
+  ADD CONSTRAINT sale_ibfk_1 FOREIGN KEY (ean) REFERENCES products (ean);
 
 ALTER TABLE sale_product
   ADD CONSTRAINT sale_product_ibfk_1 FOREIGN KEY (product_id) REFERENCES products (id),
